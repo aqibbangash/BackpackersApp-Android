bec81dda889aed8ba38d599b1f8dc9a81bc1f3e3
NAILA\Naila Kayani <nailakayani2011@hotmail.com>
2017-05-01 03:05:47 -0700

    added radio button in userRegistration java file

--- app/src/main/java/com/kit/backpackers/project_kit/UserRegistration/RegistrationActivity.java
+++ app/src/main/java/com/kit/backpackers/project_kit/UserRegistration/RegistrationActivity.java
@@ -2,14 +2,24 @@ package com.kit.backpackers.project_kit.UserRegistration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.kit.backpackers.project_kit.R;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+private RadioGroup radioGroup;
+    private RadioButton radioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+        radioGroup=(RadioGroup)findViewById(R.id.radioGender);
+
+
+
     }
 }
